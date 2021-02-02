bfca9c1f732ddc13d18e625e9acb437561aed5e1
Farouk <faroukkisuule55@gmail.com>
2021-02-01 21:01:21 -0500

    Added prompt that will collect team information

--- app.js
+++ app.js
@@ -9,3 +9,99 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+function promptUser(answers) {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "what is your role?",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+    ]).then(function (res) {
+        // should use switch case instead of if/else starting here
+        console.log(res)
+        if (res.role === "Engineer") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "github",
+                    type: "input",
+                    message: "What is your github Username?"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                }
+            ]).then(function (engineerRes) {
+                var newEngineer = new Engineer(engineerRes.name, engineerRes.email, uniqueId, engineerRes.github);
+                uniqueId = uniqueId + 1; // could be "uniqueId++"
+                console.log(newEngineer);
+                // run promptUser (called recursion) so that you can add multiple Engineers and id changes incrementally
+                teamArray.push(newEngineer);
+                addUser();
+                
+            });
+
+        } else if (res.role === "Intern") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                },
+                {
+                    name: "school",
+                    type: "input",
+                    message: "Where did you graduate from college?"
+                }
+            ]).then(function (internRes) {
+                var newIntern = new Intern(internRes.name, internRes.email, uniqueId, internRes.school);
+                uniqueId = uniqueId + 1; // could be "uniqueId++"
+                console.log(newIntern)
+                teamArray.push(newIntern);
+                addUser();
+            });
+        } else if (res.role === "Manager") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                },
+                {
+                    name: "office",
+                    type: "input",
+                    message: "What is your office number?"
+                }
+            ]).then(function (managerRes) {
+                var newManager = new Manager(managerRes.name, managerRes.email, uniqueId, managerRes.office);
+                uniqueId = uniqueId + 1; // could be "uniqueId++"
+                console.log(newManager);
+                teamArray.push(newManager);
+                addUser();
+            });
+        };
+        // should use switch case instead of if/else up until this point
+
+    })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+};
